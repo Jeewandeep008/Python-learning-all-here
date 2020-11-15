219f7d8a9fffcfcc9086299123ddb28a759e0327
JEEWAN DEEP TETE <jeewandeep008@gmail.com>
2020-11-15 07:57:52 +0530

    updates

---
+++ CLASS.py
@@ -0,0 +1,12 @@
+# CLASS
+
+class mouse:
+    def logitech(self):
+        logitech_g430 = '7.1 Surround sound headset'
+        print((logitech_g430))
+
+
+# OBJECT
+com = mouse()
+
+mouse.logitech(com)
